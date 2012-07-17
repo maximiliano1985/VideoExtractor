957a510b2acd4408b390fcbce3aafa5f080f8390
Maximiliano Giorgio <maximiliano_giorgio@yahoo.it>
2012-07-17 14:25:07 +0100

    First commit, simple working version

---
+++ Readme.md
@@ -0,0 +1,20 @@
+## DESCRIPTION:
+
+This software is used to extract part of a video. After compiling it, use the software typing on your terminal:
+
+$ ./VidExtract <InputVideo> <OutputVideo>
+	
+Then right-click with the mouse the window that opens, and to control the acquisition type:
+
+- r / R : to start the acquisition
+- s / S : to stop the acquisition
+- q / Q : to quit the software
+
+## REQUIREMENTS:
+
+The software requires OpenCV: http://opencv.willowgarage.com/wiki/
+
+## COMPILATION IN MAC OSX:
+
+To compile the software in MAC OSX using Code::Blocks (http://www.codeblocks.org/), please follow the link: http://opencv.willowgarage.com/wiki/CodeBlocks
+
---
+++ src/main.cpp
@@ -0,0 +1,89 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <cv.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int  g_slider_position = 0;
+
+cv::VideoCapture g_capture;
+
+using namespace std;
+
+
+void onTrackbarSlide(int pos, void*){
+    g_capture.set( CV_CAP_PROP_POS_FRAMES, pos );
+}
+
+int main( int argc, char** argv ){
+
+    std::string videoName, videoOut;
+
+    if( argc < 2 ){
+        std::cout << "To use this program type in your shell:\n./VidExtract <InputVideo>" << std::endl;
+        return 1;
+    }
+    else if( argc == 2 )
+    {
+        videoOut = "extractedVideo.asf";
+    }
+    else if( argc == 3 )
+    {
+        videoOut = argv[2];
+    }
+    else
+    {
+        std::cout << "To use this program type in your shell:\n./VidExtract <InputVideo> <OutputVideo>" << std::endl;
+        return 1;
+    }
+
+    char code = (char)-1;
+
+    videoName = argv[1];
+    g_capture = videoName;
+    cv::Mat frame;
+    int frameWidth  = (int)(g_capture.get(CV_CAP_PROP_FRAME_WIDTH));
+    int frameHeight = (int)(g_capture.get(CV_CAP_PROP_FRAME_HEIGHT));
+    int frames      = (int)(g_capture.get(CV_CAP_PROP_FRAME_COUNT));
+
+    cv::VideoWriter output( videoOut, CV_FOURCC( 'D', 'I', 'V', '4' ), 10., cv::Size(frameWidth, frameHeight), true);
+
+    cv::namedWindow( videoName, CV_WINDOW_AUTOSIZE );
+
+    if( frames != 0 )
+        cv::createTrackbar( "Position", videoName, &g_slider_position, frames, onTrackbarSlide, NULL);
+
+    std::cout << "Left-click on the video, and press r/R to start the extraction, or q/Q to quit" << std::endl;
+
+    bool recording = false;
+
+    while(1) {
+        g_capture >> frame;
+
+        if(frame.empty()) break;
+
+        cv::imshow( videoName, frame );
+        cv::setTrackbarPos( "Position", videoName, ++g_slider_position );
+
+        if( recording )
+            output << frame;
+
+        code = (char)cv::waitKey(30);
+        if( code == 27 || code == 'q' || code == 'Q' ) break;
+        if( code == 'r' || code == 'R' )
+        {
+            recording = true;
+            std::cout << "Recording, press s/S to stop" << std::endl;
+        }
+        if( code == 's' || code == 'S' )
+        {
+            recording = false;
+            std::cout << "Stop recording" << std::endl;
+        }
+    }
+    return 0;
+}
+
